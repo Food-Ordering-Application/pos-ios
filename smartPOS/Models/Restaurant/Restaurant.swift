42dd80cab8fb0a9a868ab5256d16b8b7196be453
Van Hiep <levanhiep.95.95@gmail.com>
2021-04-15 16:04:53 +0700

    fix some icon && layout for lower version iOS

---
+++ smartPOS/Models/Restaurant/Restaurant.swift
@@ -0,0 +1,21 @@
+//
+//  Restaurant.swift
+//  smartPOS
+//
+//  Created by I Am Focused on 14/04/2021.
+//  Copyright © 2021 Clean Swift LLC. All rights reserved.
+//
+
+import Foundation
+struct Restaurant {
+    var id: String
+    var merchant: Merchant
+    var name: String
+    var imageUrl: String
+    var videoUrl: String
+    var numRate: Int
+    var rating: Float
+    var area: String
+    var isActive: Bool
+    
+}
