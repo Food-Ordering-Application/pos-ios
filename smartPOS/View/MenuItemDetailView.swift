e5177a38f8f1bdac131aa79aa12e69b3ca99eaef
Van Hiep <levanhiep.95.95@gmail.com>
2021-04-15 20:24:25 +0700

    add radio topping selection in menuitemdetail dialog popup

--- smartPOS/View/MenuItemDetailView.swift
+++ smartPOS/View/MenuItemDetailView.swift
@@ -9,6 +9,16 @@
 import UIKit
 import SwiftEntryKit
 
+struct RadioGroup {
+    var title: String =  "Group 1"
+    var radioItems: [RadioItem] = [RadioItem(name: "Topping 1"), RadioItem(name: "Topping 2")]
+}
+
+struct RadioItem {
+    var name: String = "Topping 1"
+}
+
+
 class MenuItemDetailView: UIView {
     
     @IBOutlet var contenView: UIView!
@@ -17,6 +27,17 @@ class MenuItemDetailView: UIView {
     @IBOutlet weak var lbPrice: UILabel!
     @IBOutlet weak var lbWeight: UILabel!
     @IBOutlet weak var imageItem: UIImageView!
+    @IBOutlet weak var tableViewTopping: UITableView!
+    
+    var selectedIndexPaths = [IndexPath]()
+    
+    var toppingSelection: [RadioGroup] = [
+        RadioGroup(title: "Group 1"),
+        RadioGroup(title: "Group 2"),
+        RadioGroup(title: "Group 3"),
+        RadioGroup(title: "Group 4")
+    ]
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -38,7 +59,6 @@ class MenuItemDetailView: UIView {
         if let menuItem = data {
             self.lbName!.text = menuItem.name
             self.lbPrice!.text = String(menuItem.price)
-            
         }
     }
     
@@ -47,6 +67,21 @@ class MenuItemDetailView: UIView {
         clipsToBounds = true
         layer.cornerRadius = 15
         btnAdd.layer.cornerRadius = 8
+        self.setupTableViewTopping()
+    }
+    
+    fileprivate func setupTableViewTopping() {
+        self.tableViewTopping.separatorStyle = .none
+        self.tableViewTopping.allowsMultipleSelection = true
+        self.tableViewTopping.estimatedRowHeight = UITableView.automaticDimension
+        self.tableViewTopping.estimatedSectionHeaderHeight = UITableView.automaticDimension
+        // Register TableView Cell
+        self.tableViewTopping.register(RadioTableViewCell.nib, forCellReuseIdentifier: RadioTableViewCell.identifier)
+        self.tableViewTopping.register(SelectionHeaderView.self, forHeaderFooterViewReuseIdentifier: SelectionHeaderView.className)
+        self.tableViewTopping.dataSource = self
+        self.tableViewTopping.delegate = self
+        // Update TableView with the data
+        self.tableViewTopping.reloadData()
     }
     @IBAction func doAddMenuItem(_ sender: Any) {
         print("Iam doAddMemuItem")
@@ -54,3 +89,82 @@ class MenuItemDetailView: UIView {
     }
     
 }
+
+extension MenuItemDetailView: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return toppingSelection.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.toppingSelection[section].radioItems.count
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if let menu = LeftMenu(rawValue: indexPath.row) {
+//            self.changeViewController(menu)
+//        }
+        
+        let selectedIndexPathAtCurrentSection = selectedIndexPaths.filter({ $0.section == indexPath.section})
+
+        for indexPath in selectedIndexPathAtCurrentSection {
+            tableView.deselectRow(at: indexPath, animated: true)
+            if let indexOf = selectedIndexPaths.firstIndex(of: indexPath) {
+                selectedIndexPaths.remove(at: indexOf)
+            }
+        }
+        selectedIndexPaths.append(indexPath)
+
+    }
+    
+    // On DeSelection
+     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+
+        if let index = selectedIndexPaths.firstIndex(of: indexPath) {
+            selectedIndexPaths.remove(at: index)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        self.tableViewTopping.cellForRow(at: indexPath)?.setSelected(true, animated: true)
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+        self.tableViewTopping.cellForRow(at: indexPath)?.setSelected(false, animated: true)
+        return indexPath
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: SelectionHeaderView.className) as! SelectionHeaderView
+        header.text = toppingSelection[section].title
+        header.displayMode = PresetsDataSource.displayMode
+        return header
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RadioTableViewCell.identifier, for: indexPath) as? RadioTableViewCell else { fatalError("xib doesn't exist") }
+
+        cell.selectionStyle = .none
+        cell.setData(self.toppingSelection[indexPath.section].radioItems[indexPath.row])
+//        cell.setData(self.menus[indexPath.row])
+//        // Highlighted color
+//        let myCustomSelectionColorView = UIView()
+//        myCustomSelectionColorView.backgroundColor = #colorLiteral(red: 0.9333369732, green: 0.4588472247, blue: 0.2666652799, alpha: 0.161368649)
+//        cell.selectedBackgroundView = myCustomSelectionColorView
+        return cell
+    }
+    // MARK: - Selection
+    
+
+}
+
---
+++ smartPOS/View/TableViewCell/RadioTableViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  RadioTableViewCell.swift
+//  smartPOS
+//
+//  Created by I Am Focused on 15/04/2021.
+//  Copyright © 2021 Clean Swift LLC. All rights reserved.
+//
+
+import UIKit
+
+class RadioTableViewCell: UITableViewCell {
+    class var identifier: String { return String.className(self) }
+    class var nib: UINib { return UINib(nibName: identifier, bundle: nil) }
+    
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var radioButton: LTHRadioButton! = {
+        let r = LTHRadioButton()
+        r.translatesAutoresizingMaskIntoConstraints = false
+        return r
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    private let selectedColor   = #colorLiteral(red: 1, green: 0.2705882353, blue: 0.2274509804, alpha: 0.8211163372)
+    private let deselectedColor = UIColor.lightGray
+    
+    
+    
+    func update(with color: UIColor) {
+        backgroundColor             = color
+        radioButton.selectedColor   = color == .darkGray ? .white : selectedColor
+        radioButton.deselectedColor = color == .darkGray ? .lightGray : deselectedColor
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            return radioButton.select(animated: animated)
+        }
+        
+        radioButton.deselect(animated: animated)
+    }
+    
+    open func setData(_ data: RadioItem?) {
+//        if let menu = data {
+//            self.lbName?.text = menu.name
+//        }
+        print(data!)
+    }
+}
